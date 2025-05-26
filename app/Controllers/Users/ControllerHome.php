4da66368ef7127b0bc2e1ecc6ba25ca253afdee2
israsaqoba <111202315287@mhs.dinus.ac.id>
2025-05-26 21:28:26 +0700

    1st

---
+++ app/Controllers/Users/ControllerHome.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers\Users;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class ControllerHome extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
